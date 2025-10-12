8f3d288fd637c7c896672838a2dd5c20cf4d5512
naveen kumar <kymar7850817933@gmail.com>
2025-10-12 13:23:10 +0530

    fixed image path issue

--- frontend/src/landing_page/Home/Hero.js
+++ frontend/src/landing_page/Home/Hero.js
@@ -6,7 +6,7 @@ function Hero() {
         <>
         <div className='container-fluid mb-5 pt-5'>
             <div className='row p-5 text-center'>
-                <img src='media/images/heroimage.png' alt='hero-image' className='w-75 m-auto mb-5'/>
+                <img src='media/images/heroImage.png' alt='hero-image' className='w-75 m-auto mb-5'/>
                 <h1 className='mt-3 text-secondary'>Invest in everything</h1>
                 <p className='fs-4'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
                 <Link to="/signup" className='btn btn-primary m-auto' style={{width:300}}>Sign Up For Free</Link>
